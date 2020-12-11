295183a3ffb48487b89987bebe29af1adaac0564
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-11 18:16:14 +0100

    fixing docker

--- scrypta-connector/README.md
+++ scrypta-connector/README.md
@@ -2,17 +2,17 @@
 
 Run bash script to run mainnet idanode:
 ```
-bash docker.sh
+bash docker/docker.sh
 ```
 
 Or use `-testnet` flag to run in testnet mode:
 ```
-bash docker.sh -testnet
+bash docker/docker.sh -testnet
 ```
 
 If you want to rebuild the docker image pass `-rebuild` flag:
 ```
-bash docker.sh -rebuild
+bash docker/docker.sh -rebuild
 ``
 
 # Bootstrap blockchain and idanode (only for mainnet)
--- scrypta-connector/docker/docker.sh
+++ scrypta-connector/docker/docker.sh
@@ -3,6 +3,7 @@
 cd docker
 if [[ "$@" =~ "-rebuild" ]]
 then
+    echo "Rebuilding Docker Image"
     docker build --no-cache -t scrypta:idanode .
 else
     docker build -t scrypta:idanode .
@@ -15,5 +16,7 @@ then
 else
     echo "Running Scrypta IdaNode inside Docker, in mainnet mode."
     docker run --restart=unless-stopped -d --name=idanode -dit -p 3001:3001 scrypta:idanode
+    docker exec -it -w /opt/ idanode bash bootstrap_blockchain.sh
+    docker exec -it -w /opt/ idanode bash bootstrap_idanode.sh
     docker restart idanode
 fi
