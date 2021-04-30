739af7843dd93c9decb9d3de23f9511c88d8a32e
Samuel Sramko <samuel.sramko@tatum.io>
2021-04-30 11:38:30 +0200

    NFT - support for royalties read endpoint

--- nft-connector/module/src/NftController.ts
+++ nft-connector/module/src/NftController.ts
@@ -1,4 +1,4 @@
-import {BadRequestException, Body, Get, HttpCode, HttpStatus, Param, Post} from '@nestjs/common';
+import {BadRequestException, Body, Get, HttpCode, HttpStatus, Param, Post, Put} from '@nestjs/common';
 import {NftService} from './NftService';
 import {NftError} from './NftError';
 import {
@@ -16,6 +16,8 @@ import {
 import {PathAddressContractAddressChain} from './dto/PathAddressContractAddressChain';
 import {PathTokenIdContractAddressChain} from './dto/PathTokenIdContractAddressChain';
 import {PathChainTxId} from './dto/PathChainTxId';
+import {UpdateCashbackErc721} from '@tatumio/tatum/dist/src/model/request/UpdateCashbackErc721';
+import {CeloUpdateCashbackErc721} from '@tatumio/tatum/dist/src/model/request/CeloUpdateCashbackErc721';
 
 export abstract class NftController {
     protected constructor(protected readonly service: NftService) {
@@ -48,6 +50,15 @@ export abstract class NftController {
         }
     }
 
+    @Get('/v3/nft/royalty/:chain/:contractAddress/:tokenId')
+    public async getRoyaltyErc721(@Param() path: PathTokenIdContractAddressChain) {
+        try {
+            return await this.service.getRoyaltyErc721(path.chain, path.tokenId, path.contractAddress);
+        } catch (e) {
+            throw new NftError(`Unexpected error occurred. Reason: ${e.message || e.response?.data || e}`, 'nft.error');
+        }
+    }
+
     @Post('/v3/nft/transaction')
     @HttpCode(HttpStatus.OK)
     public async transactionErc721(@Body() body: CeloTransferErc721 | EthTransferErc721) {
@@ -69,6 +80,22 @@ export abstract class NftController {
     public async mintErc721(@Body() body: CeloMintErc721 | EthMintErc721) {
         try {
             return await this.service.mintErc721(body);
+        } catch (e) {
+            if (['Array', 'NftError', 'ValidationError'].includes(e.constructor.name)) {
+                throw new BadRequestException(e);
+            }
+            if (e.constructor.name === 'TatumError') {
+                throw e;
+            }
+            throw new NftError(`Unexpected error occurred. Reason: ${e.message || e.response?.data || e}`, 'nft.error');
+        }
+    }
+
+    @Put('/v3/nft/royalty')
+    @HttpCode(HttpStatus.OK)
+    public async updateRoyaltyErc721(@Body() body: CeloUpdateCashbackErc721 | UpdateCashbackErc721) {
+        try {
+            return await this.service.updateCashbackForAuthor(body);
         } catch (e) {
             if (['Array', 'NftError', 'ValidationError'].includes(e.constructor.name)) {
                 throw new BadRequestException(e);
--- nft-connector/module/src/NftService.ts
+++ nft-connector/module/src/NftService.ts
@@ -1,4 +1,5 @@
 import {PinoLogger} from 'nestjs-pino';
+import BigNumber from 'bignumber.js';
 import {NftError} from './NftError';
 import {
     CeloBurnErc721,
@@ -14,24 +15,29 @@ import {
     EthTransferErc721,
     prepareBscBurnBep721SignedTransaction,
     prepareBscDeployBep721SignedTransaction,
-    prepareBscMintBep721SignedTransaction,
-    prepareBscMintMultipleBep721SignedTransaction,
+    prepareBscMintBep721SignedTransaction, prepareBscMintBepCashback721SignedTransaction,
+    prepareBscMintMultipleBep721SignedTransaction, prepareBscMintMultipleCashbackBep721SignedTransaction,
     prepareBscTransferBep721SignedTransaction,
+    prepareBscUpdateCashbackForAuthorErc721SignedTransaction,
     prepareCeloBurnErc721SignedTransaction,
-    prepareCeloDeployErc721SignedTransaction,
-    prepareCeloMintErc721SignedTransaction,
+    prepareCeloDeployErc721SignedTransaction, prepareCeloMintCashbackErc721SignedTransaction,
+    prepareCeloMintErc721SignedTransaction, prepareCeloMintMultipleCashbackErc721SignedTransaction,
     prepareCeloMintMultipleErc721SignedTransaction,
     prepareCeloTransferErc721SignedTransaction,
+    prepareCeloUpdateCashbackForAuthorErc721SignedTransaction,
     prepareEthBurnErc721SignedTransaction,
-    prepareEthDeployErc721SignedTransaction,
-    prepareEthMintErc721SignedTransaction,
+    prepareEthDeployErc721SignedTransaction, prepareEthMintCashbackErc721SignedTransaction,
+    prepareEthMintErc721SignedTransaction, prepareEthMintMultipleCashbackErc721SignedTransaction,
     prepareEthMintMultipleErc721SignedTransaction,
     prepareEthTransferErc721SignedTransaction,
+    prepareEthUpdateCashbackForAuthorErc721SignedTransaction,
     TransactionHash
 } from '@tatumio/tatum';
 import erc721_abi from '@tatumio/tatum/dist/src/contracts/erc721/erc721_abi';
 import Web3 from 'web3';
 import {Transaction, TransactionReceipt} from 'web3-eth';
+import {CeloUpdateCashbackErc721} from '@tatumio/tatum/dist/src/model/request/CeloUpdateCashbackErc721';
+import {UpdateCashbackErc721} from '@tatumio/tatum/dist/src/model/request/UpdateCashbackErc721';
 
 export abstract class NftService {
 
@@ -51,6 +57,18 @@ export abstract class NftService {
         const c = new (await this.getClient(chain, await this.isTestnet())).eth.Contract(erc721_abi, contractAddress);
         try {
             return {data: await c.methods.tokenURI(token).call()};
+        } catch (e) {
+            this.logger.error(e);
+            throw new NftError(`Unable to obtain information for token. ${e}`, 'nft.erc721.failed');
+        }
+    }
+
+    public async getRoyaltyErc721(chain: Currency, token: string, contractAddress: string) {
+        // @ts-ignore
+        const c = new (await this.getClient(chain, await this.isTestnet())).eth.Contract(erc721_abi, contractAddress);
+        try {
+            const [addresses, values] = await Promise.all([c.methods.tokenCashbackRecipients(token).call(), c.methods.tokenCashbackValues(token).call()]);
+            return {addresses, values: values.map(c => new BigNumber(c).dividedBy(1e18).toString(10))};
         } catch (e) {
             this.logger.error(e);
             throw new NftError(`Unable to obtain information for token. ${e}`, 'nft.erc721.failed');
@@ -113,15 +131,28 @@ export abstract class NftService {
         const testnet = await this.isTestnet();
         let txData;
         const {chain} = body;
+        const provider = (await this.getNodesUrl(chain, testnet))[0];
         switch (chain) {
             case Currency.ETH:
-                txData = await prepareEthMintErc721SignedTransaction(body, (await this.getNodesUrl(chain, testnet))[0]);
+                if (!body.authorAddresses) {
+                    txData = await prepareEthMintErc721SignedTransaction(body, provider);
+                } else {
+                    txData = await prepareEthMintCashbackErc721SignedTransaction(body, provider);
+                }
                 break;
             case Currency.BSC:
-                txData = await prepareBscMintBep721SignedTransaction(body, (await this.getNodesUrl(chain, testnet))[0]);
+                if (!body.authorAddresses) {
+                    txData = await prepareBscMintBep721SignedTransaction(body, provider);
+                } else {
+                    txData = await prepareBscMintBepCashback721SignedTransaction(body, provider);
+                }
                 break;
             case Currency.CELO:
-                txData = await prepareCeloMintErc721SignedTransaction(testnet, body as CeloMintErc721, (await this.getNodesUrl(chain, testnet))[0]);
+                if (!body.authorAddresses) {
+                    txData = await prepareCeloMintErc721SignedTransaction(testnet, body as CeloMintErc721, provider);
+                } else {
+                    txData = await prepareCeloMintCashbackErc721SignedTransaction(testnet, body as CeloMintErc721, provider);
+                }
                 break;
             default:
                 throw new NftError(`Unsupported chain ${chain}.`, 'unsupported.chain');
@@ -137,15 +168,52 @@ export abstract class NftService {
         const testnet = await this.isTestnet();
         let txData;
         const {chain} = body;
+        const provider = (await this.getNodesUrl(chain, testnet))[0];
         switch (chain) {
             case Currency.ETH:
-                txData = await prepareEthMintMultipleErc721SignedTransaction(body, (await this.getNodesUrl(chain, testnet))[0]);
+                if (!body.authorAddresses) {
+                    txData = await prepareEthMintMultipleErc721SignedTransaction(body, provider);
+                } else {
+                    txData = await prepareEthMintMultipleCashbackErc721SignedTransaction(body, provider);
+                }
                 break;
             case Currency.BSC:
-                txData = await prepareBscMintMultipleBep721SignedTransaction(body, (await this.getNodesUrl(chain, testnet))[0]);
+                if (!body.authorAddresses) {
+                    txData = await prepareBscMintMultipleBep721SignedTransaction(body, provider);
+                } else {
+                    txData = await prepareBscMintMultipleCashbackBep721SignedTransaction(body, provider);
+                }
                 break;
             case Currency.CELO:
-                txData = await prepareCeloMintMultipleErc721SignedTransaction(testnet, body as CeloMintMultipleErc721, (await this.getNodesUrl(chain, testnet))[0]);
+                if (!body.authorAddresses) {
+                    txData = await prepareCeloMintMultipleErc721SignedTransaction(testnet, body as CeloMintMultipleErc721, provider);
+                } else {
+                    txData = await prepareCeloMintMultipleCashbackErc721SignedTransaction(testnet, body as CeloMintMultipleErc721, provider);
+                }
+                break;
+            default:
+                throw new NftError(`Unsupported chain ${chain}.`, 'unsuported.chain');
+        }
+        if (body.signatureId) {
+            return {signatureId: await this.storeKMSTransaction(txData, chain, [body.signatureId], body.index)};
+        } else {
+            return this.broadcast(chain, txData);
+        }
+    }
+
+    public async updateCashbackForAuthor(body: CeloUpdateCashbackErc721 | UpdateCashbackErc721): Promise<TransactionHash | { signatureId: string }> {
+        const testnet = await this.isTestnet();
+        let txData;
+        const {chain} = body;
+        switch (chain) {
+            case Currency.ETH:
+                txData = await prepareEthUpdateCashbackForAuthorErc721SignedTransaction(body, (await this.getNodesUrl(chain, testnet))[0]);
+                break;
+            case Currency.BSC:
+                txData = await prepareBscUpdateCashbackForAuthorErc721SignedTransaction(body, (await this.getNodesUrl(chain, testnet))[0]);
+                break;
+            case Currency.CELO:
+                txData = await prepareCeloUpdateCashbackForAuthorErc721SignedTransaction(testnet, body as CeloUpdateCashbackErc721, (await this.getNodesUrl(chain, testnet))[0]);
                 break;
             default:
                 throw new NftError(`Unsupported chain ${chain}.`, 'unsuported.chain');
