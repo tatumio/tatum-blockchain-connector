d41b834adeb93a2a1d8554eeb836b4c62aff3117
Ivan Sramko <isramko@gmail.com>
2021-05-27 19:21:37 +0200

    XDC: review

--- erc20-connector/module/src/dto/PathAddressContractAddressChain.ts
+++ erc20-connector/module/src/dto/PathAddressContractAddressChain.ts
@@ -3,10 +3,10 @@ import { PathChain } from "./PathChain";
 
 export class PathAddressContractAddressChain extends PathChain {
   @IsNotEmpty()
-  @Length(42, 42)
+  @Length(42, 43)
   public contractAddress: string;
 
   @IsNotEmpty()
-  @Length(42, 42)
+  @Length(42, 43)
   public address: string;
 }
--- erc20-connector/module/src/dto/PathTokenIdContractAddressChain.ts
+++ erc20-connector/module/src/dto/PathTokenIdContractAddressChain.ts
@@ -3,7 +3,7 @@ import { PathChain } from "./PathChain";
 
 export class PathTokenIdContractAddressChain extends PathChain {
   @IsNotEmpty()
-  @Length(42, 42)
+  @Length(42, 43)
   public contractAddress: string;
 
   @IsNotEmpty()
