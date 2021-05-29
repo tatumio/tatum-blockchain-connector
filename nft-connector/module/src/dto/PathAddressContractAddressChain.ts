e540660bd57560242fad7638fd0b332496421d1a
Samuel Sramko <samuel.sramko@tatum.io>
2021-05-29 12:37:03 +0200

    fix NFT validation length on address

--- nft-connector/module/src/dto/PathAddressContractAddressChain.ts
+++ nft-connector/module/src/dto/PathAddressContractAddressChain.ts
@@ -1,13 +1,13 @@
 import {IsNotEmpty, Length} from 'class-validator';
 import {PathChain} from './PathChain';
 
-export class PathAddressContractAddressChain extends PathChain{
+export class PathAddressContractAddressChain extends PathChain {
 
     @IsNotEmpty()
     @Length(36, 42)
     public contractAddress: string;
 
     @IsNotEmpty()
-    @Length(18, 18)
+    @Length(18, 43)
     public address: string;
 }
